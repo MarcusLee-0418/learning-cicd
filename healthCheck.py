bb9aed3f8d099aa0981aa14e54f1bdcfd42be4ca
MarcusLee-0418 <marcuslee11223@gmail.com>
2026-09-03 16:46:38 +0800

    healthCheck update

--- healthCheck.py
+++ healthCheck.py
@@ -2,9 +2,10 @@ import json, datetime, sys, os
 SERVERS = ["web-01","web-02","db-01","api-01"]
 def main():
   print("start executing health check")
-  token = os.environ.get("MY_SECRET_KEY")
+  token = os.environ.get("SECRET_TOKEN")
   if not token:
     print("Error: cannot find the security token")
+    sys.exit(1)
   if token == "hkmaPassWord@123456":
     print(f"access successfully. Token: {token}")
 if __name__ == "__main__":
